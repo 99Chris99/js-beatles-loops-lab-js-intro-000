2ce4fe6eb726cfec5271d84a5af5f18946d59d30
Chris Hodge <chrishodge87@gmail.com>
2019-06-28 13:32:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@
 function theBeatlesPlay(musicians, instruments){
 var i = []
 for (i = 0; i < musicians; i++)
-return (musicians + "plays" + instruments)
+return musicians + "plays" + instruments
 }
